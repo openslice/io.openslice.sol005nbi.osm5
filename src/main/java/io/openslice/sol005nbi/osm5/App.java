c7811a6edc53f3c1facc6baa1deb96f426ce2956
Christos Tranoris <tranoris@gmail.com>
2019-07-23 11:21:46 +0300

    Initial commit

---
+++ src/main/java/io/openslice/sol005nbi/osm5/App.java
@@ -0,0 +1,13 @@
+package io.openslice.sol005nbi.osm5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
